ac5500ff23bf836421df6b94da074477d0e30ea3
Rodrigo Alberto Queiroz Costa <rodrigo.costa@spassu.com.br>
2025-10-06 22:05:28 -0300

    commit inicial

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO pautas (titulo, descricao, data_criacao) VALUES
-('teste1','teste1', CURRENT_TIMESTAMP()),
-('teste2','teste2', CURRENT_TIMESTAMP()),
-('teste3','teste3', CURRENT_TIMESTAMP());
